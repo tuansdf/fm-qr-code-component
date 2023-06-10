43de25bf8a3445044c9c9afab5f6d84cde560c4e
tuansdf <103062682+tuansdf@users.noreply.github.com>
2023-06-10 16:20:03 +0700

    add final

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,23 @@ import classes from "./page.module.scss";
 export default function Home() {
   return (
     <>
-      <main className={classes["main"]}>Home</main>
+      <article className={classes["main"]}>
+        <img
+          className={classes["image"]}
+          loading="lazy"
+          decoding="async"
+          src="/images/image-qr-code.png"
+        />
+        <div className={classes["body"]}>
+          <h1 className={classes["title"]}>
+            Improve your front-end skills by building projects
+          </h1>
+          <p className={classes["subtitle"]}>
+            Scan the QR code to visit Frontend Mentor and take your coding
+            skills to the next level
+          </p>
+        </div>
+      </article>
       <Attribution />
     </>
   );
